3de630f512674e3247ad36c80d412b9fc1c3abf3
gurami <guramidgebuadze@gmail.com>
2022-11-13 22:15:33 +0400

    homework finished

--- script.js
+++ script.js
@@ -60,8 +60,178 @@ const ifAnyPeopleIS30Age = people.some((x) => x.age > 30);
 
 // --- დავალება N2 ---
 // flatten arrays into a single array
+let flattenArray = [];
 function flatten(arr) {
   // your code here
+  arr.forEach((x) => {
+    if (Array.isArray(x)) {
+      flattenArray.push(...x);
+    } else {
+      flattenArray.push(x);
+    }
+  });
+  return flattenArray;
 }
-const arrays = [["first", "second"], [1, 2, 3], [true]];
-console.log(flatten(arrays)); // ['first', 'second', 1, 2, 3, true],
+const arrays = [["first", "second"], [1, 2, 3], [true], 1];
+
+// console.log(flatten(arrays)); // ['first', 'second', 1, 2, 3, true],
+
+// --- დავალება3 ---
+// implement polyfill functions for map, filter, every, reduce
+// map
+let arr = [1, 2, 3, 4];
+
+function myMap(arr1) {
+  let mapArr = [];
+
+  arr1.forEach((el) => {
+    mapArr.push(el);
+  });
+
+  return mapArr;
+}
+myMap(arr);
+
+// filter
+let arr10 = [1, 2, 4, 6];
+let fn1 = (x) => x % 2 === 0;
+
+function myFilter(arr, fn) {
+  let filtered = [];
+  arr.forEach((i) => {
+    console.log(i);
+    if (fn(i)) {
+      filtered.push(i);
+    }
+  });
+  return filtered;
+}
+let arr11 = myFilter(arr10, fn1);
+
+// every
+let arr2 = [1, 2, 3, 4];
+let fn = (x) => x > 2;
+
+function myEvery(arr3, fn1) {
+  let everyArr = [];
+
+  arr3.forEach((el) => {
+    if (fn1(el)) {
+      everyArr.push(el);
+    }
+  });
+
+  return everyArr.length === arr3.length;
+}
+myEvery(arr2, fn);
+
+// reduce
+let arr4 = [];
+
+function myReduce(arr5) {
+  let reducedArr = function (arr6) {
+    if (!arr6.length) {
+      throw new TypeError("myReduce is called on null or undefined");
+    } else if (typeof arr6[0] === "number") {
+      return 0;
+    } else if (typeof arr6[0] === "string") {
+      return "";
+    }
+  };
+
+  arr5.forEach((el) => {
+    reducedArr += el;
+  });
+  return reducedArr;
+}
+
+// --- დავალება4 ---
+// There’s a ladder object that allows to go up and down:
+let ladder = {
+  step: 0,
+
+  up: function () {
+    this.step++;
+    return this;
+  },
+  down: function () {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    // shows the current step
+    alert(this.step);
+    return this;
+  },
+};
+// Now, if we need to make several calls in sequence, can do it like this:
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+// ladder.down();
+// ladder.showStep(); // 0
+// Modify the code of up, down and showStep to make the calls chainable, like this:
+// ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
+
+// --- დავალება 5 ---
+// you have an array of people
+const people1 = [
+  {
+    name: "George",
+    age: 25,
+  },
+  {
+    name: "Michael",
+    age: 26,
+  },
+  {
+    name: "Anna",
+    age: 25,
+  },
+  {
+    name: "Kate",
+    age: 27,
+  },
+  {
+    name: "James",
+    age: 27,
+  },
+  {
+    name: "Peter",
+    age: 32,
+  },
+  {
+    name: "Jacob",
+    age: 27,
+  },
+];
+
+// write function groupBy(array, key), which will group people by the given key
+function groupBy(array, key) {
+  // your code goes here...
+  return array.reduce((acc, curr) => {
+    if (Object.keys(acc).includes(`${curr[key]}`)) {
+      acc[curr[key]].push(curr);
+    } else {
+      acc[curr[key]] = [curr];
+    }
+    return acc;
+  }, {});
+}
+
+// for 'people' array, if I group by 'age', this function must
+// return the following object
+const grouped = groupBy(people1, "age");
+
+//  =>
+// {
+//   '25': [ { name: 'George', age: 25 }, { name: 'Anna', age: 25 } ],
+//   '26': [ { name: 'Michael', age: 26 } ],
+//   '27': [
+//   { name: 'Kate', age: 27 },
+//   { name: 'James', age: 27 },
+//   { name: 'Jacob', age: 27 }
+//   ],
+//   '32': [ { name: 'Peter', age: 32 } ]
+//   }
